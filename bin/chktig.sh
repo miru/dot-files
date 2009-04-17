98e5549885e2aa1c22ed89467f348a8d3936175a
miru <mirupreza@gmail.com>
2009-04-17 10:47:36 +0900

    modified:   bin/chktig.sh 	modified:   bin/chkwig.sh

--- bin/chktig.sh
+++ bin/chktig.sh
@@ -2,10 +2,10 @@
 
 setenv CMD1DIR "~/app/TwitterIrcGateway/"
 setenv CMD1CMD "TwitterIrcGatewayCLI"
-setenv CMD1OPT "--port=16668 --interval=30 -interval-directmessage=60 --post-fetch-mode=true --broadcast-update=false --encoding=utf-8 --enable-replies-check=true --interval-replies=30 --ignore-watch-error=true --client-message-wait=100 --broadcast-update-message-is-notice=true"
+setenv CMD1OPT "--port=16668 --encoding=utf-8"
 setenv CMD2CMD "Twitter"
 
-@ CMD1NO=`ps aux | grep ug3 | grep $CMD2CMD | grep -v grep | wc -l | sed s/" "//g`
+@ CMD1NO=`ps aux | grep miru | grep $CMD2CMD | grep -v grep | wc -l | sed s/" "//g`
 
 if ( $CMD1NO == 0 ) then
   echo "Detect lost $CMD1CMD ..."
--- bin/chkwig.sh
+++ bin/chkwig.sh
@@ -4,7 +4,7 @@ setenv CMD1DIR "~/app/net-irc/"
 setenv CMD1CMD "./examples/wig.rb"
 setenv CMD2CMD "wig.rb"
 
-@ CMD1NO=`ps aux | grep ug3 | grep $CMD2CMD | grep -v grep | wc -l | sed s/" "//g`
+@ CMD1NO=`ps aux | grep miru | grep $CMD2CMD | grep -v grep | wc -l | sed s/" "//g`
 
 if ( $CMD1NO == 0 ) then
   echo "Detect lost $CMD1CMD ..."
