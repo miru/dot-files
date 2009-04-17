16411cdeafb97905702df36982b9bcf3dac70661
miru <mirupreza@gmail.com>
2009-04-17 18:00:05 +0900

    vimperator.xpi nightly build

--- bin/chkmob.sh
+++ bin/chkmob.sh
@@ -3,7 +3,7 @@
 setenv CMD1CMD "./mobirc"
 setenv CMD1OPT "--daemonize"
 
-@ CMD1NO=`ps aux | grep ug3 | grep $CMD1CMD | grep -v grep | wc -l| sed s/" "//g`
+@ CMD1NO=`ps aux | grep miru | grep $CMD1CMD | grep -v grep | wc -l| sed s/" "//g`
 
 if ( $CMD1NO == 0 ) then
   echo "Detect lost $CMD1CMD ..."
