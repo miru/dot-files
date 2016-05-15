cfa5b59efd91b381e1f6db65a4472a6968401eb7
miru <mirupreza@gmail.com>
2016-05-15 23:12:12 +0900

    Change pushbullet message.

--- bin/fastcheck_narou.sh
+++ bin/fastcheck_narou.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
+# -*- coding:utf-8 -*-
 
+# Set check ID "ATOMID:nID"
+CHECK_ID="372556:n4830bu 282802:n9902bn 615833:n0885dc"
+
+# Set Pushbullet token
 PUSHBULLET_TOKEN=<PUSHBULLET token>
 
-CACHE_DIR=~/tmp/fastcheck_narou_cache
-CHECK_ID="372556:n4830bu 282802:n9902bn"
+# Set "narou init" dir
 NAROU_DIR=~/narou
 
-###
+# Set cache dir
+CACHE_DIR=~/tmp/fastcheck_narou_cache
+
+####
+export LANG=ja_JP.UTF-8
+export LANGUAGE=ja_JP
+####
+
+### main ###
 if [ ! -d $CACHE_DIR ]; then
     mkdir $CACHE_DIR
 fi
-
 ###
 pushd $NAROU_DIR >/dev/null 2>&1
-
-
 ###
+
+### loop ###
 for ID in $CHECK_ID
 do
     AID=`echo $ID | awk -F: '{print $1}'`
@@ -29,15 +40,13 @@ do
     curl -s  http://api.syosetu.com/writernovel/$AID.Atom | grep CDATA > $CACHE_FILE.new
 
     diff $CACHE_FILE $CACHE_FILE.new >/dev/null 2>&1
-    DIFF_NUM=`echo $?`
-
-    if [ $DIFF_NUM -ne 0 ]; then
-	#narou up $NID >/dev/null 2>&1
-	RES=`narou up $NID`
+    if [ $? -ne 0 ]; then
+	RES=`/usr/local/bin/narou up $NID | egrep ".mobi" | sed "s/.mobi を出力しました//g"`
+	TIT=`echo $RES | sed "s/\[.*\] //g"`
 	curl --header "Access-Token: $PUSHBULLET_TOKEN" --header "Content-Type: application/json" \
-	     --data-binary "{\"body\":\"$RES\",\"title\":\"小説更新\",\"type\":\"note\"}" \
+	     --data-binary "{\"body\":\"$RES が更新されました\",\"title\":\"小説更新 $TIT\",\"type\":\"note\"}" \
 	     --request POST https://api.pushbullet.com/v2/pushes >/dev/null 2>&1
-	narou ma $NID >/dev/null 2>&1
+	/usr/local/bin/narou ma $NID >/dev/null 2>&1
     fi
     
     cp -f $CACHE_FILE.new $CACHE_FILE
