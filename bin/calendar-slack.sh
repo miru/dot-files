e420341f5386b57af0bc363c87113933b6c21cc5
mirupreza <mirupreza@gmail.com>
2017-03-04 21:11:54 +0900

    new file:   calendar-slack.sh

---
+++ bin/calendar-slack.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export SLACK_WEBHOOK="https://hooks.slack.com/services/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"
+
+$BODY=`LANG=C date +http://miniature-calendar.com/wp-content/uploads/%Y/%m/%y%m%d%a.jpg | tr '[A-Z]' '[a-z]'`
+/usr/bin/curl -X POST --data-urlencode "payload={\"channel\": \"#general\", \"username\": \"MINIATURE CALENDAR\", \"text\": \"$BODY\", \"icon_emoji\": \":date:\"}" $SLACK_WEBHOOK
+
+# EOF
+
